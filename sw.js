a4cb8bc40bc7b54a82288844d4f0a86ae4adbbfa
ieduh3iwhh <2016353772@qq.com>
2025-04-05 20:45:24 +0800

    Site updated: 2025-04-05 20:45:24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1743856787415";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1743857114783";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
